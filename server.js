cf14fd6a9bc9263a9cf047b920e3f2a1c475f41a
Charlie Carter <charliec1665@gmail.com>
2022-09-22 13:57:32 -0400

    functioning POST requests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // heroku link https://morning-coast-38766.herokuapp.com/api/animals
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
@@ -8,6 +10,12 @@ const PORT = process.env.PORT || 3001;
 
 // instantiate server
 const app = express();
+
+// MIDDLEWARE: functions mounted to server
+// parse incoming string or array data // this method is a builtin Express.js method
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -50,6 +58,18 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal (body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
 // get method requires two arguments, the route the client will fetch from
     // and the callback function that will execute every time the route is accessed with a GET request
 app.get('/api/animals', (req, res) => {
@@ -71,6 +91,19 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 })
+
+// .post routes listen for POST requests instead of GET requests. POST requests represent a client requesting
+    // that the server ACCEPT data.
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+});
 
 // set the server up to listen using the listen() method
 app.listen(PORT, () => {
